17a01731e134a088e394b51ab3c25208f6501a7d
SirNooby <159674595+Sir-Nooby@users.noreply.github.com>
2024-11-23 21:39:42 +0000

    beta 3

--- script.js
+++ script.js
@@ -38,3 +38,31 @@ document.getElementById("task-button").addEventListener('click', function() {
         alert('Please enter a task!');
     }
 });
+
+
+
+
+
+
+
+// Shop the systems
+shades = document.getElementById("acc-shades")
+beanie = document.getElementById("acc-beanie")
+tshirt = document.getElementById("acc-tshirt")
+
+dragon_image = document.getElementById("dragon-image")
+
+shades.addEventListener("click", function() {
+    console.log("shades clicked")
+    dragon_image.src = "Contents/Sprites/Characters/firedragon_shades.gif"
+});
+
+beanie.addEventListener("click", function() {
+    console.log("beanie clicked")
+    dragon_image.src = "Contents/Sprites/Characters/firedragon_beanie.gif"
+});
+
+tshirt.addEventListener("click", function() {
+    console.log("tshirt clicked")
+    dragon_image.src = "Contents/Sprites/Characters/firedragon_tshirt.gif"
+});
